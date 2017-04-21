477cc8612303c7f6b140fa85e957ed6bcbc04897
Dolisinho <raicard@live.it>
2017-04-21 09:53:52 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>test</title>
+</head>
+<body>
+    <h1 class="title">Test title text</h1>
+    <div class="foo">Test class text</div>
+    <ul>
+        <li></li>
+        <li></li>
+    </ul>
+    <div class="bar">Antonio Pierro</div>
+</body>
+</html>
